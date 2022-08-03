c6ada5b30f1b990e1f2a699e3b9908f9b9a662a5
Kris Watts <kris.watts@gravwell.io>
2022-08-03 14:00:21 -0600

    fixing build for missing experimental features and docker files

--- dockerfiles/brokenperms/run.sh
+++ dockerfiles/brokenperms/run.sh
@@ -5,6 +5,15 @@ if [ "$base" == "" ]; then
 	echo "we need the gravwell:perms image as a base"
 	exit -1
 fi
-docker build --compress --squash -t gravwell:brokenperms .
+
+SQUASH="--squash"
+exp=`docker version -f '{{.Server.Experimental}}'`
+if [ "$exp" == "false" ]; then
+	echo "your docker instance isn't running with experimental features"
+	echo "We need this in order to squash the image during build"
+	SQUASH=""
+fi
+
+docker build --compress $SQUASH -t gravwell:brokenperms .
 docker save gravwell:brokenperms | gzip > $TGT
 echo "Base image is at $TGT"
--- dockerfiles/go.mod
+++ dockerfiles/go.mod
@@ -3,10 +3,11 @@ module main
 go 1.15
 
 require (
+	github.com/gravwell/gencert v3.2.3+incompatible // indirect
 	github.com/gravwell/gravwell/v3 v3.5.0 // indirect
 	github.com/gravwell/ingest v3.2.2+incompatible // indirect
 	github.com/gravwell/manager/v3 v3.3.12 // indirect
 	github.com/nerdalert/nflow-generator v0.0.0-20220501044009-5cc1c43806c2 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 )
